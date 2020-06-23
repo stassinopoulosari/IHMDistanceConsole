79f4ddc968d04c7717b777e04088f14b742663b1
Ari Stassinopoulos <stassinopoulosari@gmail.com>
2020-06-23 15:21:13 -0700

    Add password reset

--- assets/login.js
+++ assets/login.js
@@ -3,17 +3,46 @@
 
   var auth = firebase.auth(),
     $submitButton = document.getElementById("dd-login-submit"),
+    $resetPasswordButton = document.getElementById("dd-login-reset-password"),
     $email = document.getElementById("dd-login-email"),
     $password = document.getElementById("dd-login-password"),
     $errorMessage = document.getElementById("dd-login-errorMessage");
 
   $errorMessage.style.display = "none";
 
+  var resetErrorMessageStyle = () => {
+    $errorMessage.classList.remove("alert-success");
+    $errorMessage.classList.add("alert-danger");
+    $errorMessage.style.display = "none";
+  };
 
+  var resetPassword = $resetPasswordButton.onclick = () => {
+    resetErrorMessageStyle();
+    var email = $email.value;
+    if(email.trim() == "") {
+      $errorMessage.innerText = "Please enter your email in order to reset your password.";
+      $errorMessage.style.display = "block";
+      return;
+    }
+
+    auth.sendPasswordResetEmail(email, {
+      url:"https://ihmdc.ari-s.com/",
+      handleCodeInApp: false
+    }).then(() => {
+      $errorMessage.innerText = "Assuming you have an account, I've sent you a password reset link. Please do be patient and check your spam folder for it.";
+      $errorMessage.classList.remove("alert-danger");
+      $errorMessage.classList.add("alert-success");
+      $errorMessage.style.display = "block";
+    }).catch(() => {
+      $errorMessage.innerText = "Sorry, the password reset failed. Please talk to Ari to sort this out using the big red button at the bottom of the page.";
+      $errorMessage.style.display = "block";
+      return;
+    });
+  }
 
   var tryLogin = $submitButton.onclick = () => {
 
-    $errorMessage.style.display = "none";
+    resetErrorMessageStyle();
 
     $submitButton.disabled = true;
 
@@ -24,7 +53,7 @@
       .catch((error) => {
         loginError = error;
         $submitButton.disabled = false;
-        var errorMessage = (loginError.message || "The login failed");
+        var errorMessage = (loginError.message || "The login failed, sorry.");
         $errorMessage.innerText = errorMessage;
         $errorMessage.style.display = "block";
       })
